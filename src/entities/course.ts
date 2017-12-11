7e5e14f07df1793ef8167ee98372101c1d594a78
hafsa <hafssa.yargueit@esprit.tn>
2017-12-11 20:27:49 +0100

    added entity course

---
+++ src/entities/course.ts
@@ -0,0 +1,13 @@
+export class Course{
+
+  constructor(
+    public name :string,
+    public description :string,
+    public startdate :string,
+    public enddate :string,
+    public id?:number
+    // public userId?:number
+  ){}
+
+}
+
